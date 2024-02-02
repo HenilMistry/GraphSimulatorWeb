3c052785bf9b88260fddaa79c508394d1dbe6334
henilMistry <henilmistry74496@gmail.com>
2024-02-03 00:43:13 +0530

    Made a working Node Tool and UndirectedEdge Tool

--- Asset/main.js
+++ Asset/main.js
@@ -8,13 +8,45 @@ const mouse = {
   x: innerWidth / 2,
   y: innerHeight / 2,
 };
+let Nodes = [];
+let UdEdges = [];
+let click = 0;
+let firstNode = -1;
 
 const colors = ["#2185C5", "#7ECEFD", "#FFF6E5", "#FF7F66"];
 
 // Event Listeners
-addEventListener("mousemove", (event) => {
+addEventListener("click", (event) => {
   mouse.x = event.clientX;
   mouse.y = event.clientY;
+  // Console lines are just for testing 
+  // console.log("MOUSE x:"+mouse.x+" y:"+mouse.y);
+  if(ActiveTool == Tools.NODE) {
+    Nodes.push(new Node(mouse.x,mouse.y,20,"Red",c));
+    console.log(Nodes);
+  } else if(ActiveTool == Tools.UndirectedEdge) {
+    if(Nodes.length > 0) {
+      Nodes.forEach((node, index) => {
+        if(distance(node.x,node.y,mouse.x,mouse.y) <= node.radius) {
+          // Just for testing...
+          // console.log("Collision with "+index);
+          click++;
+          if(click%2==0) {
+            // it means it's a second click..
+            UdEdges.push(new EdgeHelper(firstNode,node,c));
+            click = 0;
+            firstNode = null;
+          } else {
+            firstNode = node;
+          }
+        }
+      });
+    } else {
+      alert("No nodes are on the Canvas!");
+      selectTool("None");
+    }
+    console.log(UdEdges);
+  }
 });
 
 addEventListener("resize", () => {
@@ -24,36 +56,10 @@ addEventListener("resize", () => {
   init();
 });
 
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
-  }
-
-  draw() {
-    c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.fillStyle = this.color;
-    c.fill();
-    c.closePath();
-  }
-
-  update() {
-    this.draw();
-  }
-}
-
 // Implementation
 let objects;
 function init() {
   objects = [];
-
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
-  }
 }
 
 // Animation Loop
@@ -61,30 +67,19 @@ function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
 
-  c.fillStyle = "Red";
-  c.fillRect(0,0,canvas.width, canvas.height);
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+  // Rendering all the UdEdges. Edges on the canvas 
+  UdEdges.forEach((edge) => {
+    edge.update();
+  });
+
+  // Rendering all nodes on the canvas 
+  Nodes.forEach((node)=>{
+    node.update();
+  });
 }
 
 init();
 animate();
-
-/**
-
- *  @function randomIntFromRange Picks a random integer within a range
- *  @function randomColor Picks a random color
- *  @function dispatch Find the distance between two points
- **/ 
-
-function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function randomColor(colors) {
-  return colors[Math.floor(Math.random() * colors.length)];
-}
 
 function distance(x1, y1, x2, y2) {
   const xDist = x2 - x1;
