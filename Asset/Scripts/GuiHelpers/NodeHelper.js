704ff8c9ee2d6b2c73eef1d7dfe46788582a0278
henilMistry <henilmistry74496@gmail.com>
2024-02-04 00:00:25 +0530

    Added a label for node

--- Asset/Scripts/GuiHelpers/NodeHelper.js
+++ Asset/Scripts/GuiHelpers/NodeHelper.js
@@ -1,6 +1,7 @@
 // A class for rendering the node for graph on canvas 
 class Node {
-    constructor(x,y,radius,color,context) {
+    constructor(label,x,y,radius,color,context) {
+        this.label = label;
         this.x = x;
         this.y = y;
         this.radius = radius;
@@ -10,6 +11,9 @@ class Node {
     
     draw() {
         this.c.beginPath();
+        this.c.font = 'italic 18px Arial';
+        this.c.fillStyle = "Black";
+        this.c.fillText(this.label,this.x-(this.radius),this.y-(this.radius));
         this.c.arc(this.x,this.y,this.radius,Math.PI*2,false);
         this.c.fillStyle = this.color;
         this.c.fill();
